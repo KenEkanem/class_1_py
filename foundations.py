6daed68a3468b834c0945b85a48448fe99ed0421
KenEkanem <kennedyekanem@gmail.com>
2025-05-23 22:17:29 +0100

    Initial Commit

---
+++ foundations.py
@@ -0,0 +1,75 @@
+# Created a python file
+# Show how to run a python file using different methods
+# create a simple python program
+
+
+# DATA TYPES
+# -- This section is for chukwudi
+
+# Number 
+
+# Integers
+# 2, 4, -2, 0, 56356737, -11123
+
+# Float
+# 0.5, 34.89, -365.02, 64563.7686739
+
+# Complex Numbers
+# 1 + j, 2 + 4j,
+
+
+# Strings
+# one or more characters
+
+# 'Kennedy'
+# 'Peace'
+# 'This is a python class'
+# 'we are having the time of our lives'
+# "Hows your first python tut going"
+# "123456"
+# '-345.64'
+
+
+# BOOLEAN
+
+# True
+# False 
+
+# LIST
+# Diff 
+
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# ['YAM', 'CASSAVA', 'COCOYAM', 'WATERYAM']
+# ['Spain', 'Turkey', 'Switzerland', 'Nigeria', 'Ghana', 'Kenya']
+# ['banana', -10, 23.456, False]
+
+
+# Tuple
+# Diff
+# immutable
+
+('Kennedy', 'Peace', 'Gideon', 'Inyene', 'Chuks')
+# how to wrap a selected item at once in quotes
+
+# -- Chkuwudi stops here
+
+
+# --- Peace
+
+# DICTIONARY
+
+{
+    'first_name' : "Kennedy",
+    'last_name' : "Ekanem",
+    'state' : "Ebonyi",
+    "age" : 12,
+    "buzz_words" : ["teaching", "python", "Devrel", "infra", "open source"],
+    "married?" : True
+}
+
+# set
+# What is the keyword for a set?
+
+
+{2, 4, 1, 7}
+
